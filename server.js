7460f2a0b2a2b5fd1f0f791be80875c6076ffa2e
Rob Hoover <rtrhoover@gmail.com>
2022-03-04 13:34:14 -0800

    code review and clean up

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
-const register = require('./controllers/register')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
 
 const db = knex({
     client: 'pg',
@@ -20,70 +23,12 @@ const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res)=> {
-    res.send('success');
-})
-
-app.use(cors({
-    origin: ['http://localhost:3000'],
-    credentials: true
-}));
-
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-   
-app.listen(80, function () {
-console.log('CORS-enabled web server listening on port 80')
-})
-
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-})
-
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('Not Found')
-            }
-        })
-        .catch(err => res.status(400).json('Error Getting User'))
-})
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries)
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
-
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
-})
+app.listen(3000, () => { console.log('app is running on port 3000') })
