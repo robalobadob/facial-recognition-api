f1877e00af644b34fa0c6ee0603ed2037c849928
Rob Hoover <rtrhoover@gmail.com>
2022-03-04 13:04:52 -0800

    added controller file structure

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register')
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -55,34 +57,7 @@ app.post('/signin', (req, res) => {
         .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email,
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0].email,
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
