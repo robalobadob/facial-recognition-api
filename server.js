ad2b560eeb59a34b8719119b81e22ceefb45fbd3
Rob Hoover <rtrhoover@gmail.com>
2022-04-06 10:46:35 -0700

    fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-// app.use(cors());
+// `app.use(cors());`
 
 app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
